fc0f89fa23c1690b166e2020c37f23cf84c9069b
Louis-Ye <yeluyupt@gmail.com>
2013-12-28 00:42:32 +0800

    waiting threads scan-lan.sh

--- scan-lan.sh
+++ scan-lan.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+function is_IP_there {
+	ping -c 4 $1 > /dev/null && echo "$1"
+}
+
 subNet=$1
 from=$2
 to=$3
+echo "Following IP may exist:"
+
+pidA=()
 cur=$from
-
-function is_IP_there {
-	ping -c 1 $1 > /dev/null && echo "$1 exists"
-}
-
 while [[ $cur -le $to ]] ; do
 	IP_addr="$subNet$cur"
 	is_IP_there $IP_addr &
+	pidA+=($!)
 	cur=$(($cur+1))
+done
+
+for item in ${pidA[@]} ; do
+	wait $item
 done
 
